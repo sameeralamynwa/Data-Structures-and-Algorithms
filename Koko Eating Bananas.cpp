90937cab23ed580db403e70c69bda95a92057a9a
Sameer Alam <sameeralamynwa@gmail.com>
2022-06-04 11:56:10 +0530

    Update Koko Eating Bananas.cpp

--- Koko Eating Bananas.cpp
+++ Koko Eating Bananas.cpp
@@ -3,8 +3,7 @@ public:
     int minEatingSpeed(vector <int> &piles, int h) {
         int low = 1, high = *max_element(piles.begin(), piles.end());
         while(low < high){
-            int mid = (low + high) >> 1;
-            int hoursRequired = 0;
+            int mid = (low + high) >> 1, hoursRequired = 0;
             for(int i = 0; i < (int) piles.size(); ++i){
                 hoursRequired += 1 + (piles[i] - 1) / mid;
             }
