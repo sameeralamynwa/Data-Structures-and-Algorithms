9fcbbe68258ff321ee09a2f1d569f4811df78b0a
Sameer Alam <sameeralamynwa@gmail.com>
2022-07-02 09:23:52 +0530

    Update Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts.cpp

--- Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts.cpp
+++ Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts.cpp
@@ -1,29 +1,18 @@
 class Solution {
 private:
-    const int mod = 1e9 + 7;
+    static const int mod = 1e9 + 7;
+    
 public:
-    int maxArea(int h, int w, vector<int>& horizontalCuts, vector<int>& verticalCuts) {
-        horizontalCuts.insert(horizontalCuts.begin(), 0);
-        horizontalCuts.insert(horizontalCuts.begin(), h);
-        
-        verticalCuts.insert(verticalCuts.begin(), 0);
-        verticalCuts.insert(verticalCuts.begin(), w);
-        
-        sort(horizontalCuts.begin(), horizontalCuts.end());
-        sort(verticalCuts.begin(), verticalCuts.end());
-        
-        long long maxArea = 0;
-        int maxWidth = 0;
-        
-        for(int i = 0; i + 1 < verticalCuts.size(); ++i){
-            maxWidth = max(maxWidth, verticalCuts[i + 1] - verticalCuts[i]);
-        }        
-        
-        for(int i = 0; i + 1 < horizontalCuts.size(); ++i){
-            int length = horizontalCuts[i + 1] - horizontalCuts[i];
-            maxArea = max(maxArea, (long long) length * maxWidth);
+    int computeMaxGap(vector <int> &positions, int length){
+        sort(positions.begin(), positions.end());
+        int maxGap = max(*positions.begin(), length - positions.back());
+        for(int i = 0; i + 1 < (int) positions.size(); ++i){
+            maxGap = max(maxGap, positions[i + 1] - positions[i]);
         }
-        
-        return maxArea % mod;
+        return maxGap;
+    }
+
+    int maxArea(int h, int w, vector<int>& horizontalCuts, vector<int>& verticalCuts) {
+        return (long long) computeMaxGap(horizontalCuts, h) * computeMaxGap(verticalCuts, w) % mod;
     }
 };
